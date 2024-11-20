f23e99b068f1287ad19146f9d3b3146ddbb2ada5
Naluephan <por.za1713@gmail.com>
2024-11-20 14:20:27 +0700

    first commit

---
+++ newlaravel/newlavel/app/Repositories/Eloquent/BaseRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\Base;
+use App\Repositories\BaseRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class BaseRepository.
+ */
+class BaseRepository implements BaseRepositoryInterface
+{
+    /**
+     * UserRepository constructor.
+     *
+     * @param Base $model
+     */
+    protected $model;
+    public function __construct(Model $model)
+    {
+        $this->model =$model;
+    }
+    public function all()
+    {
+        return $this->model->all();
+    }
+    public function find($id): ?Model
+    {
+        return $this->model->find($id);
+    }
+    public function create(array $data): Model
+    {
+        return $this->model->create($data);
+    }
+    public function update($id, array $data): Model
+    {
+        $model = $this->model->find($id);
+        $model->update($data);
+        return $model;
+    }
+    public function delete($id)
+    {
+        return $this->model->find($id)->delete();
+    }
+}
